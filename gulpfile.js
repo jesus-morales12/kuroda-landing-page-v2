0018eb51cc57a5740be2586e07b69dd6b680bcf3
Jesus <jesusdmk@hotmail.com>
2018-09-13 18:06:02 -0600

    september update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
-
-gulp.task('style', function () {
+gulp.task('styles', function(){
     gulp.src('css/style.css')
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('build'))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'))
 });
 
-
-gulp.task('watch', function () {
-    gulp.watch('css/style.css', ['style']);
-}); 
+gulp.task('watch',function(){
+    gulp.watch('css/style.css', ['styles']);
+});
